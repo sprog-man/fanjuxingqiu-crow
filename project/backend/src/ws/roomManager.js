3ad48a5ac31409feea9116260c054db0b2b06d8b
sprog-man <2484511957@qq.com>
2026-05-19 23:08:55 +0800

    fix: member 对象增加 openid 字段 + findMemberByOpenid 方法

--- project/backend/src/ws/roomManager.js
+++ project/backend/src/ws/roomManager.js
@@ -1,15 +1,19 @@
 const crypto = require('crypto');
 
 class Room {
-  constructor(hostId, hostNickname, hostAvatar) {
+  constructor(hostId, hostNickname, hostAvatar, hostOpenid) {
     this.code = crypto.randomBytes(3).toString('hex').toUpperCase().slice(0, 6);
-    this.members = [{ id: hostId, nickname: hostNickname, avatar: hostAvatar || '', isHost: true }];
+    this.members = [{
+      id: hostId, nickname: hostNickname, avatar: hostAvatar || '',
+      openid: hostOpenid || '', isHost: true
+    }];
     this.gameState = null;
     this.createdAt = Date.now();
     this.lastActivity = Date.now();
   }
   findMember(socketId) { return this.members.find(m => m.id === socketId); }
-  addMember(id, nickname, avatar) { this.members.push({ id, nickname, avatar: avatar || '', isHost: false }); this.touch(); }
+  findMemberByOpenid(openid) { return this.members.find(m => m.openid === openid); }
+  addMember(id, nickname, avatar, openid) { this.members.push({ id, nickname, avatar: avatar || '', openid: openid || '', isHost: false }); this.touch(); }
   removeMember(id) { this.members = this.members.filter(m => m.id !== id); this.touch(); }
   get host() { return this.members.find(m => m.isHost); }
   touch() { this.lastActivity = Date.now(); }
@@ -20,8 +24,8 @@ class RoomManager {
     this.rooms = new Map();
     this._cleanupTimer = setInterval(() => this._cleanup(), 60000);
   }
-  createRoom(hostId, hostNickname, hostAvatar) {
-    const room = new Room(hostId, hostNickname, hostAvatar);
+  createRoom(hostId, hostNickname, hostAvatar, hostOpenid) {
+    const room = new Room(hostId, hostNickname, hostAvatar, hostOpenid);
     this.rooms.set(room.code, room);
     return room;
   }
